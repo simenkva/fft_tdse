8de46a200b760d106615eb4746ed5f879a22df2f
simenkva <simen.kvaal@gmail.com>
2022-12-06 13:02:52 +0100

    Updated Caroline code

--- simulate1d_caroline.py
+++ simulate1d_caroline.py
@@ -48,33 +48,16 @@ if verbose:
 
 
 try:
-    p = config['potential']['p']
-except:
-    p = []
-
-try:
+#    p = config['potential']['p']
     temp = config['potential']['U']
     Vfun0 = eval(temp)
-
     if verbose:
-        print(f'Potential parameters: {p}')
+#        print(f'Potential parameters: {p}')
         print(f'Model potential: {temp}')
-except KeyError:
-    Vfun0 = lambda x: -np.exp(-0.1*x**2)
-
+except:
+    Vfun0 = lambda x: -1.0*np.exp(-0.1*x**2)
     if verbose:
         print(f'Model potential is default.')
-
-try:
-    temp = config['potential']['ground_state_guess']
-    ground_state_guess = eval(temp)
-
-    if verbose:
-        print(f'Ground state guess: {temp}')
-except KeyError:
-    ground_state_guess = lambda x: np.exp(-x**2)
-    if verbose:
-        print(f'Ground state guess is default (gaussian)')
 
 
 try:
@@ -98,7 +81,6 @@ try:
 except:
     gs_tol = 1e-7
 
-
 try:
     t_final = config['integration']['t_final']
 except:
@@ -115,32 +97,33 @@ except:
 try:
     # Set up laser field
     E0 = config['pulse']['E0']
-    t0 = config['pulse']['t0']
+    tau = config['pulse']['tau']
     om = config['pulse']['om']
-    nc = config['pulse']['nc']
+    T = config['pulse']['T']
 except:
     E0, tau, om, T = 0.25, 20.5, 1.0/(2*np.pi), 50
+    if verbose:
+        print(f'Laser parameters are default.')
 
-try: # this is fine, caroline.
+try:
     # Set up kinetic energy operator
     mu = config['kinetic']['mu']
 except:
     mu = 1
-if verbose:
-    print(f'Mass mu = {mu}')
+
 
 if verbose:
-    print(f'Number of grid points: {ng}**3')
-    print(f'Simulation domain: [-{L},{L}]**3')
+    print(f'Number of grid points: {ng}')
+    print(f'Simulation domain: [-{L},{L}]')
     print(f'Number of grid points (ground-state calc): {gs_ng}')
     print(f'Simulation domain (ground-state calc): [-{gs_L},{gs_L}]')
     print(f'Termination tolerance for ground-state calc: {gs_tol}')
     print(f'Time interval: [0,{t_final}]')
     print(f'Time step: {dt}')
     print(f'Number of density inspections: {n_inspect}')
-#    print(f'Laser parameters E0, tau, om, T = {E0, tau, om, T}')
-    print(f'Laser parameters E0, t0, om, nc = {E0, t0, om, nc}')
-    print(f'Mass mu = {mu}')
+    print(f'Laser parameters E0, tau, om, T = {E0, tau, om, T}')
+#    print(f'Laser parameters E0, t0, om, nc = {E0, t0, om, nc}')
+    print(f'Mass mu = {mu}')    
 
 
 # Set up a fairly large grid
@@ -159,29 +142,18 @@ Vfun = lambda xx: Vfun0(xx[0]) # the grid stores the nodes for each dimension in
 x = grid.x[0]
 
 
-def compute_ground_state(L2,ng2,Vfun,mu=1,psi_guess = None):
+def compute_ground_state(L2,ng2,Vfun):
     """ Compute ground state wavefunction on a suitable grid,
-    and then extrapolate to the computationa grid.
-    mu = mass of particle, defaults to 1. """
+    and then extrapolate to the computationa grid. """
 
     grid2 = FourierGrid([-L2],[L2],[ng2])
     x = grid2.x[0]
 
-    ham = FourierHamiltonian(grid2, Vfun = Vfun, Tfun = lambda k: T_standard(k,mu))
+    ham = FourierHamiltonian(grid2, Vfun = Vfun)
 
     gs = GroundStateComputer(ham)
-    #gs.setInitialGuess(np.exp(-x**2/2))
-    # Note that this breaks if you change the potential ...
+    gs.setInitialGuess(np.exp(-x**2/2))
     #gs.setInitialGuess(np.exp(-(x-p[2])**2/2)+np.exp(-(x+p[2])**2/2))
-
-    # I have changed to this:
-    if psi_guess is None:
-        # Default is a gaussian. Maybe random is better?
-        gs.setInitialGuess(np.exp(-x**2/2))
-    else:
-        # use guess supplied as parameter
-        gs.setInitialGuess(psi_guess(x))
-
     sigma = np.min(Vfun([x]))
     E = gs.invit(sigma = sigma)
 
@@ -197,7 +169,7 @@ def compute_ground_state(L2,ng2,Vfun,mu=1,psi_guess = None):
     return gs.wf.interpolate(grid).psi
 
 # Compute ground state on a smaller grid, then extrapolate.
-psi0 = compute_ground_state(gs_L,gs_ng,Vfun,mu=mu,psi_guess = ground_state_guess)
+psi0 = compute_ground_state(gs_L,gs_ng,Vfun)
 
 
 # In[3]:
@@ -205,12 +177,12 @@ psi0 = compute_ground_state(gs_L,gs_ng,Vfun,mu=mu,psi_guess = ground_state_guess
 
 # Set up laser field
 #E0, tau, om, T = 0.25, 20.5, 1.0/(2*np.pi), 50
-#Efun = lambda t: E0*np.exp(-(t-T)**2/tau**2) * np.cos(om*t)
+Efun = lambda t: E0*np.exp(-(t-T)**2/tau**2) * np.cos(om*t)
 
-Efun = lambda t: (E0*np.sin(om*np.pi*(t-t0)/nc/2)**2
-                   *np.sin(om*(t-t0))
-                   *np.heaviside(t-t0, 1.0)
-                   *np.heaviside(2*nc/om-t+t0, 1.0))
+#Efun = lambda t: (E0*np.sin(om*np.pi*(t-t0)/nc/2)**2 
+#                   *np.sin(om*(t-t0))
+#                   *np.heaviside(t-t0, 1.0)
+#                   *np.heaviside(2*nc/om-t+t0, 1.0))
 
 if figures:
     plt.figure()
@@ -254,8 +226,7 @@ wf = FourierWavefunction(grid)
 wf.setPsi(psi0,normalize=True)
 
 # Set up Hamiltonian
-# Note the mass mu
-ham = FourierHamiltonian(grid, Vfun = Vfun, Efun=Efun, Tfun = lambda k: T_standard(k,mu))
+ham = FourierHamiltonian(grid, Vfun = Vfun, Efun=Efun)
 
 # Create a Strang splitting propagator
 #dt = 0.01
@@ -312,9 +283,11 @@ if figures:
 
 
 # Save simulation results
-matlab_output = {'x': x, 't': t_range, 'psigrid': psi_hist, 'E': E_hist, 'E0': E0,
-                 't0': t0, 'om': om, 'nc': nc, 'param': p}
-savemat(f'{sim_name}.mat', matlab_output)
+#matlab_output = {'x': x, 't': t_range, 'psigrid': psi_hist, 'E': E_hist, 'E0': E0, 
+#                 't0': t0, 'om': om, 'nc': nc, 'param': p}
+# matlab_output = {'x': x, 't': t_range, 'psigrid': psi_hist, 'E': E_hist, 'E0': E0, 
+#                 'tau': tau, 'om': om, 'T': T}
+# savemat(f'{sim_name}.mat', matlab_output)
 
 
 # Create a file name
@@ -329,6 +302,4 @@ with h5py.File(fname,'w') as h5file:
     h5file.create_dataset('/parameters/grid', data=x)
     h5file.create_dataset('/wavefunctions/psigrid', data = psi_hist, compression = 'gzip')
     h5file.create_dataset('/computed/energy', data = E_hist)
-
-
 
